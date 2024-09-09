301cc1452505f9ad1fed9f4bdbf777a343089832
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-09-09 12:43:20 +0500

    Fix Resume Base Background is displaying on half screen on mobile device

--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -12,16 +12,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="grid min-h-screen place-items-center bg-gray-400">
+    <div className="min-w-[220mm] min-h-[330mm] grid place-items-center bg-gray-400">
       <h2 className="text-4xl font-bold text-light py-4">Resume Layout</h2>
+
+      {/* A4 Size (210 x 297 mm) (793.70 x 1122.51 px) */}
       <div
-        className="flex m-4 h-[297mm] w-[210mm] overflow-hidden rounded-md bg-white shadow-lg"
+        className="w-[210mm] h-[297mm] overflow-hidden rounded-md bg-white shadow-lg"
       >
-        <div className="w-1/3">
-          <Sidebar/>
-        </div>
-        <div className="w-2/3">
-          {children}
+        <div className="resume h-full flex">
+          <div className="w-1/3">
+            <Sidebar/>
+          </div>
+          <div className="w-2/3">
+            {children}
+          </div>
         </div>
       </div>
     </div>
