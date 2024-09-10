7b387178ddddfa78df135b889e69e93d85660816
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-09-10 11:01:25 +0500

    [ADDED] Interface for PersonalInfoDataType

---
+++ app/interfaces/PersonalInfo.tsx
@@ -0,0 +1,8 @@
+export default interface PersonalInfoDataType {
+    username?: string | null,
+    email?: string | null,
+    contact?: string | null,
+    address?: string | null,
+    linkedIn?: string | null,
+    portfolio?: string | null
+}
