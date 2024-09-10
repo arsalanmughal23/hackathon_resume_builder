82083b3cf747d5aea0c88e76780911d7f29d767a
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-09-10 11:00:59 +0500

    [UPDATED] components/ui (Button & Input) tsx    - Add new Attribute onClick

--- app/components/ui/Input.tsx
+++ app/components/ui/Input.tsx
@@ -4,6 +4,7 @@ export type InputTextType = {
     type?: string,
     className?: string,
     placeholder?: string,
+    onChange?: (username: string) => void,
 }
 
 export default function InputText({
@@ -11,7 +12,8 @@ export default function InputText({
     type="text",
     value,
     className,
-    placeholder
+    placeholder,
+    onChange
 } : InputTextType)
 {
     return (
@@ -20,7 +22,8 @@ export default function InputText({
             type={type}
             value={value}
             className={`${className} w-full border rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-primary-light focus:ring-primary-light focus:ring-1`} 
-            placeholder={placeholder} 
+            placeholder={placeholder}
+            onChange={(e) => onChange?.(e.target.value)}
         />
     )
 }
