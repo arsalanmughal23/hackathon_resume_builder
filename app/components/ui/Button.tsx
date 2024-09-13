e7ba35454649196f0a6faed3b2e8dceb5fcecbf4
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-09-13 14:42:40 +0500

    Fix onClick Prop on Component/ui/Button.tsx

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,21 +1,21 @@
 export type ButtonType = {
     type?: "button" | "submit" | "reset" | undefined,
     className?: string,
-    onClickHandler?: ()=>void|any,
+    onClick?: ()=>void,
     children?: React.ReactNode
 }
 
 export default function Button({
     type="button",
     className,
-    onClickHandler=()=>{},
+    onClick=()=>{},
     children,
 } : ButtonType)
 {
     return (
         <button 
             type={type}
-            onClick={() => onClickHandler()}
+            onClick={onClick}
             className={`${className} w-full border rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-primary-light focus:ring-primary-light focus:ring-1`} 
         >
             { children || 'Save' }
